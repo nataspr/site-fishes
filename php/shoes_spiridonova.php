b6a0cae2e713ab3c7f7a5543474a1b73903b9f69
Spiridonova Nataliia <spiridonova.nataliia@gmail.com>
2024-03-14 15:15:07 +0300

    fileChange

--- php/shoes_spiridonova.php
+++ php/shoes_spiridonova.php
@@ -3,7 +3,7 @@
 
 <head>
     <title>PHP Forms</title>
-
+    <link rel="stylesheet" type="text/css" href="../css/shoes.css">
 </head>
 
 <body>
@@ -16,16 +16,16 @@
         <!-- Ввести имя и выбрать ботинки -->
         <label for="shoes"> Обувь:</label>
         <select id="shoes" name="shoes">
-            <option value="boots">Ботинки</option>
-            <option value="lether">Резиновые сапоги</option>
-            <option value="sandals">Сандали</option>
+            <option value="boots" <?php if(isset($_POST['shoes']) && $_POST['shoes'] == 'boots') echo 'selected'; ?> >Ботинки</option>
+            <option value="lether" <?php if(isset($_POST['shoes']) && $_POST['shoes'] == 'lether') echo 'selected'; ?> >Резиновые сапоги</option>
+            <option value="sandals" <?php if(isset($_POST['shoes']) && $_POST['shoes'] == 'sandals') echo 'selected'; ?> >Сандали</option>
         </select><br><br>
 
         <!-- Выбрать цвет радиокнопки -->
         <label>Цвет обуви:</label><br>
-        <input type="radio" id="red" name="color" value="Красный">
+        <input type="radio" id="red" name="color" value="Красный" <?php if(isset($_POST['color']) && $_POST['color'] == 'Красный') echo 'checked'; ?> >
         <label for="red">Красный</label>
-        <input type="radio" id="white" name="color" value="Белый">
+        <input type="radio" id="white" name="color" value="Белый" <?php if(isset($_POST['color']) && $_POST['color'] == 'Белый') echo 'checked'; ?> >
         <label for="white">Белый</label><br><br>
 
         <!-- Кнопки животного + отправка -->
@@ -34,9 +34,71 @@
         <input type="submit" name="animal" value="Собака">
     </form>
 
+    <div class="output" style="border: solid black 2px; padding: 2px; margin: 2px; width:fit-content;">
     <?php
-        include './shoe_1.php';
-    ?>
+        if ($_SERVER["REQUEST_METHOD"] == "POST" || $_SERVER["REQUEST_METHOD"] == "GET") {
+            if (isset($_POST['name'], $_POST['animal'], $_POST['shoes'], $_POST['color']) && $_POST['name']!="") {
+                $name = $_REQUEST['name'];
+                $animal = $_REQUEST['animal'];
+                $color = $_REQUEST['color'];
+                $shoes = $_REQUEST['shoes'];
+    
+                // Определяем количество пар обуви на картинку в зависимости от выбранного животного
+                
+                switch ($animal) {
+                    case 'Паук':
+                        // Подсчитываем количество пар обуви
+                        $shoe_quantity = 4;
+                        break;
+                    case 'Утка':
+                        // Подсчитываем количество пар обуви
+                        $shoe_quantity = 1;
+                        break;
+                    case 'Собака':
+                        // Подсчитываем количество пар обуви
+                        $shoe_quantity = 2;
+                        break;
+                    default:
+                        $shoe_quantity = 0;
+                        break;
+                }
+    
+                // определить обувь и картинку для нее
+                switch ($shoes) {
+                    case 'boots':
+                        $shoe_image = "./../img/shoes/shoes3.png";
+                        break;
+                    case 'lether':
+                        $shoe_image = "./../img/shoes/shoes2.png";
+                        break;
+                    case 'sandals':
+                        $shoe_image = "./../img/shoes/shoes1.png";
+                        break;
+                    default:
+                        $shoe_image = "";
+                        break;
+                }
+                
+                
+                // Выводим информацию
+                echo "<h2>Результат:</h2>";
+                echo "<p>Имя: $name</p>";
+                echo "<p>Животное: $animal</p>";
+                echo "<p>Цвет обуви: $color</p>";
+                echo "<p>Количество пар обуви: $shoe_quantity</p>";
+    
+                // Выводим изображение
+                if ($shoe_image !== "") {
+                    echo "<img src='$shoe_image' alt='shoes' width= 40% height = 40%>";
+                }
+                
+            }
+            else{
+                echo "<p>Заполните форму, пожалуйста</p>";
+            }
+        }
+        
+    ?></div>
 </body>
 
 </html>
